3bbce9dd7ad8dc0cdfd0a5e1f89e8be44268902f
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-14 14:17:47 -0500

    added field to budget models and implemented budget service

--- internal/features/budgets/budget.go
+++ internal/features/budgets/budget.go
@@ -1,8 +1,9 @@
 package budgets
 
 type Budget struct {
-	ID       int       `json:"_id"`
-	UserID   int       `json:"user_id"`
-	Category string    `json:"category"`
-	Amount   float64   `json:"amount"`
+	ID       int     `json:"_id"`
+	UserID   int     `json:"user_id"`
+	Name     string  `json:"name"`
+	Category string  `json:"category"`
+	Amount   float64 `json:"amount"`
 }
--- internal/features/budgets/budgetDto.go
+++ internal/features/budgets/budgetDto.go
@@ -2,6 +2,7 @@ package budgets
 
 type BudgetDto struct {
 	UserID   int     `json:"user_id"`
+	Name     string  `json:"name"`
 	Category string  `json:"category"`
 	Amount   float64 `json:"amount"`
 }
