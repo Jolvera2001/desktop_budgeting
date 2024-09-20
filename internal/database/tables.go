6f6c0ee4e423d9ad8041052a540a5d921f285c60
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-20 12:31:05 -0500

    updated tests and related tables/services

--- internal/database/tables.go
+++ internal/database/tables.go
@@ -1,6 +1,6 @@
 package database
 
-const(
+const (
 	userTable string = `
 	CREATE TABLE IF NOT EXISTS users(
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -8,26 +8,47 @@ const(
 	name TEXT,
 	budgetPeriod INTEGER
 	);`
+
 	budgetTable string = `
 	CREATE TABLE IF NOT EXISTS budgets(
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	userId INT NOT NULL,
 	categoryId INT NOT NULL,
 	name TEXT,
-	amount INT,
+	amount REAL,
 	FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 	FOREIGN KEY (categoryId) REFERENCES categories(id) ON DELETE CASCADE
 	);`
+
 	transactionsTable string = `
 	CREATE TABLE IF NOT EXISTS transactions(
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	userId INT NOT NULL,
 	budgetId INT NOT NULL,
 	description TEXT, 
-	amount FLOAT,
+	amount REAL,
 	category TEXT,
 	date DATETIME,
 	FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 	FOREIGN KEY (budgetId) REFERENCES budgets(id) ON DELETE CASCADE
+	);`
+
+	incomeTable string = `
+	CREATE TABLE IF NOT EXISTS income(
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	userId INT NOT NULL,
+	categoryId INT NOT NULL,
+	amount REAL,
+	isRegular INT,
+	date TEXT,
+	FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+	FOREIGN KEY (budgetId) REFERENCES budgets(id) ON DELETE CASCADE
+	);`
+	categoryTable string = `
+	CREATE TABLE IF NOT EXISTS categories(
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	userId INT NOT NULL,
+	name TEXT,
+	FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 	);`
 )
