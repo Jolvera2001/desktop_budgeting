72132c3adf263ed7da0ba971a23ac5985b1081a5
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-20 12:03:05 -0500

    updated category model, created model dto, and implemented service

--- internal/features/categories/category.go
+++ internal/features/categories/category.go
@@ -1,6 +1,7 @@
 package categories
 
 type Category struct {
-	ID   int64  `json:"_id_"`
-	Name string `json:"category"`
+	ID     int64  `json:"_id_"`
+	UserID int64  `json"userId"`
+	Name   string `json:"category"`
 }
--- internal/features/categories/category_service.go
+++ internal/features/categories/category_service.go
@@ -1,27 +1,87 @@
 package categories
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type CategoryService struct {
 	Client *sql.DB
 }
 
-func (c *CategoryService) CreateCategory(dto string) (int64, error) {
+func (s *CategoryService) CreateCategory(dto CategoryDto) (int64, error) {
+	res, err := s.Client.Exec("INSERT INTO categories (userId, name) VALUES (?, ?)",
+		dto.UserID, dto.Name)
+	if err != nil {
+		return 0, fmt.Errorf("error adding category: %v", err)
+	}
 
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error adding category: %v", err)
+	}
+
+	return id, nil
 }
 
-func (c *CategoryService) GetCategory(id int64) (Category, error) {
+func (s *CategoryService) GetCategory(id int64) (Category, error) {
+	var category Category
+	row := s.Client.QueryRow("SELECT * FROM categories WHERE id = ?", id)
 
+	if err := row.Scan(&category.ID, &category.UserID, &category.Name); err != nil {
+		return Category{}, fmt.Errorf("error getting category: %v", err)
+	}
+
+	return category, nil
 }
 
-func (c *CategoryService) GetCategoryList(userId int64) ([]Category, error) {
+func (s *CategoryService) GetCategoryList(userId int64) ([]Category, error) {
+	var categories []Category
 
+	rows, err := s.Client.Query("SELECT * FROM categories WHERE userId = ?", userId)
+	if err != nil {
+		return []Category{}, fmt.Errorf("error fetching categories: %v", err)
+
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var category Category
+
+		if err := rows.Scan(&category.ID, &category.UserID, category.Name); err != nil {
+			return nil, fmt.Errorf("error fetching categories: %v", err)
+		}
+
+		categories = append(categories, category)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error while fetching categories: %v", err)
+	}
+
+	return categories, nil
 }
 
-func (c *CategoryService) UpdateCategory(update Category) error {
+func (s *CategoryService) UpdateCategory(update Category) error {
+	query := `
+	UPDATE categories
+	SET name = ?
+	WHERE id = ?
+	`
+	_, err := s.Client.Exec(query, update.Name, update.ID)
+	if err != nil {
+		return fmt.Errorf("error updating category: %v", err)
+	}
 
+	return nil
 }
 
-func (c *CategoryService) DeleteCategory(id int64) error {
+func (s *CategoryService) DeleteCategory(id int64) error {
+	_, err := s.Client.Exec("DELETE FROM categories WHERE id = ?", id)
+	if err != nil {
+		return fmt.Errorf("error deleting category")
+	}
 
+	return nil
 }
