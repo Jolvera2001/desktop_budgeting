eb677a619813f0f03a7320537d0af2304c5186cc
Jolvera2001 <jolvera6@stedwards.edu>
2024-10-10 17:48:16 -0500

    editting main layout

--- frontend/src/MainLayout.tsx
+++ frontend/src/MainLayout.tsx
@@ -1,14 +1,25 @@
 import { Outlet } from "react-router-dom"
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs"
+import { Home } from "lucide-react"
 
 function MainLayout() {
     return(
-        <div className="grid grid-cols-4 grid-rows-[auto_1fr] h-screen">
-            <div className="col-span-1 row-span-1 bg-gray-200 border-b-slate-500"></div>
-            <div className="col-span-3 row-span-1 bg-gray-200 border-b-slate-500">
+        <div className="grid grid-cols-10 grid-rows-[auto_1fr] h-screen bg-gray-200">
+            <div className="col-span-1 row-span-1 bg-gray-200 border-b-slate-300"></div>
+            <div className="col-span-9 row-span-1 bg-gray-200 border-b-slate-300 min-h-[50px]">
 
             </div>
-            <div className="col-span-1 bg-gray-200 border-b-slate-500"></div>
-            <div className="col-span-3 border-b-slate-500">
+            <div className="col-span-1 bg-gray-200 border-b-slate-300">
+                <Tabs defaultValue="home" orientation="vertical" className="w-full">
+                    <TabsList className="flex flex-col h-full space-y-2 bg-gray-100 p-2">
+                        <TabsTrigger value="home" className="w-full flex items-center justify-start gap-2">
+                            <Home size={16} />
+                            Home
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </div>
+            <div className="col-span-9 border-b-slate-500 bg-white">
                 <Outlet />
             </div>
         </div>
