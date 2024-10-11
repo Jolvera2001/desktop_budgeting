445427c144f2b30f815b40b371d928e062cab689
Jolvera2001 <jolvera6@stedwards.edu>
2024-10-11 16:19:33 -0500

    implemented category crud

--- internal/repository/categoryCrud.go
+++ internal/repository/categoryCrud.go
@@ -1,6 +1,10 @@
 package repository
 
-import "desktop_budgeting/internal/models"
+import (
+	"desktop_budgeting/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type CategoryCrudInterface interface {
 	Create(category *models.Category) (uint, error)
@@ -8,3 +12,45 @@ type CategoryCrudInterface interface {
 	Update(category *models.Category) error
 	Delete(id uint) error
 }
+
+type CategoryCrud struct {
+	Repo *gorm.DB
+}
+
+func NewCategoryCrud(repo *gorm.DB) *CategoryCrud {
+	return &CategoryCrud{Repo: repo}
+}
+
+func (c *CategoryCrud) Create(category *models.Category) (uint, error) {
+	res := c.Repo.Create(category)
+	if res.Error != nil {
+		return 0, res.Error
+	}
+	return category.ID, nil
+}
+
+func (c *CategoryCrud) Get(id uint) (*models.Category, error) {
+	var category models.Category
+	res := c.Repo.First(&category, id)
+	if res.Error != nil {
+		return &models.Category{}, res.Error
+	}
+	return &category, nil
+}
+
+func (c *CategoryCrud) Update(category *models.Category) error {
+	res := c.Repo.Save(category)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
+
+func (c *CategoryCrud) Delete(id uint) error {
+	res := c.Repo.Delete(&models.Category{}, id)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
+
